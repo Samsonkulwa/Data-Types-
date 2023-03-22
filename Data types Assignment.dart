e4a461a25c01437eae00cd32f00523e60836eded
Samsonkulwa <33673041+Samsonkulwa@users.noreply.github.com>
2023-03-22 17:24:43 +0300

    Add files via upload

---
+++ Data types Assignment.dart
@@ -0,0 +1,26 @@
+//Data types assignemnt.
+void main ()
+{
+//Integer
+int x = 93;
+print(x);
+//Double
+//Body temperature in fahrenheit
+double f = 98.6;
+print(f);
+//String
+String name = "Samson";
+print(name);
+// List
+List names = ["Beatrice","Sam","Joyce","3"];
+print(names);
+//Maps
+var identifier = {"Sam":"Username","Frank":"Passwords"};
+print(identifier);
+//Map Keys
+print(identifier.keys);
+//Map Values
+print(identifier.values);
+//Map length
+print(identifier.length);
+}
